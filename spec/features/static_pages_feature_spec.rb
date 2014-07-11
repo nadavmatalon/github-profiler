8821417001d93bd9737af2cbaa5dfcde659da440
Nadav <nadav.matalon@gmail.com>
2014-07-11 10:41:34 +0100

    added static pages content and tests

--- spec/features/static_pages_feature_spec.rb
+++ spec/features/static_pages_feature_spec.rb
@@ -6,41 +6,66 @@ describe "Static pages: " do
 
 	describe "\'root\' page" do
 		
-		it "should have the content 'StaticPages#home'" do
-			visit root_path
-			should have_content("StaticPages#home")
+		before (:each) { visit root_path }
+
+		it "should have the title \'Web Templates | Home\'" do
+			should have_title("Web Template | Home")
+		end
+
+		it "should have the content \'Web Templates\'" do
+			should have_content("WEB TEMPLATE")
 		end
 	end
 
 	describe "Home page" do
 		
-		it "should have the content 'StaticPages#home'" do
-			visit home_path
-			should have_content("StaticPages#home")
+		before (:each) { visit home_path }
+
+		it "should have the title \'Web Template | Home\'" do
+			should have_title("Web Template | Home")
+		end		
+
+		it "should have the content 'Web Template'" do
+			should have_content("WEB TEMPLATE")
 		end		
 	end
 
 	describe "About Us page" do
+
+		before (:each) { visit about_path }
 		
-		it "should have the content 'StaticPages#about'" do
-			visit about_path
-			should have_content("StaticPages#about")
+		it "should have the title \'Web Template | About\'" do
+			should have_title("Web Template | About")
+		end
+
+		it "should have the content \'About Us\'" do
+			should have_content("About Us")
 		end
 	end
 
 	describe "Contact Us page" do
 	
-		it "should have the content 'StaticPages#contact'" do
-			visit contact_path
-			should have_content("StaticPages#contact")
+		before (:each) { visit contact_path }
+
+		it "should have the title \'Web Template | Contact\'" do
+			should have_title("Web Template | Contact")
+		end
+
+		it "should have the content \'Contact Us\'" do
+			should have_content("Contact Us")
 		end
 	end
 
 	describe "Help page" do
+
+		before (:each) { visit help_path }
 		
-		it "should have the content 'StaticPages#help'" do
-			visit help_path
-			should have_content("StaticPages#help")
+		it "should have the content \'Web Template | Help\'" do
+			should have_title("Web Template | Help")
+		end
+
+		it "should have the content \'Help\'" do
+			should have_content("Help")
 		end
 	end
 end
