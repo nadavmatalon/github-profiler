0e76991821947106f25a5e8e079ce29e99583359
Nadav <nadav.matalon@gmail.com>
2014-07-11 22:08:44 +0100

    user interface

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         authentication_keys: [ :name, :username, :email ]
+         authentication_keys: [ :email ]
 end
