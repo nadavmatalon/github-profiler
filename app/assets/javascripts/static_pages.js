f273c994d880472c408b25fe6ebd1f8567c77579
Nadav <nadav.matalon@gmail.com>
2014-07-13 20:58:47 +0100

    nav-bar styling elements

---
+++ app/assets/javascripts/static_pages.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    if (document.title == "Web Template | Home") {
+        $(".home").addClass('active');
+    } else if (document.title == "Web Template | About") {
+        $(".about").addClass('active');
+    } else if (document.title == "Web Template | Contact") {
+        $(".contact").addClass('active');
+    } else if (document.title == "Web Template | Help") {
+        $(".help").addClass('active');
+    } else if (document.title == "Web Template | Sign In") {
+        $(".sign-in").addClass('active');
+    } else if (document.title == "Web Template | Sign Up") {
+        $(".sign-up").addClass('active');
+    } else if (document.title == "Web Template | Profile") {
+        $(".profile").addClass('active');
+    } else if (document.title == "Web Template | Account") {
+        $(".account").addClass('active');
+    }; 
+});
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,5 @@ module ApplicationHelper
 		base_title = "Web Template"
 		page_title.empty? ? base_title : "#{base_title} | #{page_title}"
 	end
+
 end
