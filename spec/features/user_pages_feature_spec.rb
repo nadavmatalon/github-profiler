ca252c179c8b8cb0add6d23c16ef773f88f44b64
Nadav <nadav.matalon@gmail.com>
2014-07-20 03:12:01 +0100

    updated tests

--- spec/features/user_pages_feature_spec.rb
+++ spec/features/user_pages_feature_spec.rb
@@ -85,7 +85,7 @@ describe "User Pages >" do
 			click_link 'Sign out'
   		  	should have_content 'Welcome user'
   		  	sign_in
-  		  	should have_content 'Welcome Ja'
+  		  	should have_content 'Welcome ja'
 			click_link 'Sign out'
   		  	should have_content 'Welcome user'
   		end
