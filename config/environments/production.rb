b4c1b6ce4a8b2186aac538fa8a9261d1a05feddd
Nadav <nadav.matalon@gmail.com>
2014-09-03 02:34:00 +0100

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,6 @@ Rails.application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
 
 
 
