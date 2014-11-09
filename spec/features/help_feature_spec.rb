087229cc6eaf5962f18f2e47a825197985257a30
Nadav <nadav.matalon@gmail.com>
2014-11-09 23:09:31 +0000

    code refactoring: fixed indentation

--- spec/features/help_feature_spec.rb
+++ spec/features/help_feature_spec.rb
@@ -2,11 +2,11 @@ describe "Help page" do
 
 	subject { page }
 
-    before (:each) { visit help_path }
+	before (:each) { visit help_path }
 
-  	it "should have page title \'Github Profiler | Help\'" do
-        should have_title "Github Profiler | Help"
-    end
+	it "should have page title \'Github Profiler | Help\'" do
+		should have_title "Github Profiler | Help"
+	end
 
 	it "should have the content \'GITHUB PROFILER\'" do
 		should have_content "GITHUB PROFILER"
