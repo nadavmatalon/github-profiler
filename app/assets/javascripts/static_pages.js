c0b672a68ff22ce8f3a5712d9366bd9e3aacb631
Nadav <nadav.matalon@gmail.com>
2014-11-09 21:42:20 +0000

    code refactoring: javascript of static pages

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,23 +1,23 @@
 $(document).ready(function(){
-    if (document.title == "Github Profiler | Home") {
+    if (document.title === "Github Profiler | Home") {
         $(".home").addClass('active');
-    } else if (document.title == "Github Profiler | About") {
+    } else if (document.title === "Github Profiler | About") {
         $(".about").addClass('active');
-    } else if (document.title == "Github Profiler | Contact") {
+    } else if (document.title === "Github Profiler | Contact") {
         $(".contact").addClass('active');
-    } else if (document.title == "Github Profiler | Help") {
+    } else if (document.title === "Github Profiler | Help") {
         $(".help").addClass('active');
-    } else if (document.title == "Github Profiler | Sign In") {
+    } else if (document.title === "Github Profiler | Sign In") {
         $(".sign-in").addClass('active');
-    } else if (document.title == "Github Profiler | Sign Up") {
+    } else if (document.title === "Github Profiler | Sign Up") {
         $(".sign-up").addClass('active');
-    } else if (document.title == "Github Profiler | Profile") {
+    } else if (document.title === "Github Profiler | Profile") {
         $(".profile").addClass('active');
-    } else if (document.title == "Github Profiler | Github Profile") {
+    } else if (document.title === "Github Profiler | Github Profile") {
         $(".github-profile").addClass('active');
-    } else if (document.title == "Github Profiler | Account") {
+    } else if (document.title === "Github Profiler | Account") {
         $(".account").addClass('active');
-    };
+    }
 });
 
 
