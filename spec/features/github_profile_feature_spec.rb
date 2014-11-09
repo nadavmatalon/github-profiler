6b70dba8eb57bd44e9e42a263b56eab371ad9483
Nadav <nadav.matalon@gmail.com>
2014-11-09 23:08:28 +0000

    code refactoring: fixed indentation

--- spec/features/github_profile_feature_spec.rb
+++ spec/features/github_profile_feature_spec.rb
@@ -7,9 +7,9 @@ describe "Github Profile page" do
 		click_link "Github Profile"
 	end
 
-   it "should have page title \'Github Profiler | Github Profile\'" do
-        should have_title "Github Profiler | Github Profile"
-    end
+	it "should have page title \'Github Profiler | Github Profile\'" do
+		should have_title "Github Profiler | Github Profile"
+	end
 
 	it "should have the content \'GITHUB PROFILER\'" do
 		should have_content "GITHUB PROFILER"
@@ -24,7 +24,7 @@ describe "Github Profile page" do
 			should have_content "test_username"
 		end
 	end
-	
+
 	it "should have the content \'Public Repos\'" do
 		should have_content "Public Repos"
 	end
@@ -38,13 +38,12 @@ describe "Github Profile page" do
 	end
 end
 
-
 def github_profile_sign_up
-    visit root_path
-    click_link "Sign up"
-    fill_in "user_username", with: "test_username"
-    fill_in "user_password", with: "password"
-    fill_in "user_password_confirmation", with: "password"
-    click_button "Sign up"
+	visit root_path
+	click_link "Sign up"
+	fill_in "user_username", with: "test_username"
+	fill_in "user_password", with: "password"
+	fill_in "user_password_confirmation", with: "password"
+	click_button "Sign up"
 end
 
